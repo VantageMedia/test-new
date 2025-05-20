775783c806a3fec5d7b29bc55f234c821231329c
Adam Wolfson <vantagebusinessinc@gmail.com>
2025-05-19 22:47:33 -0500

    Fix React Router: add /services route in App, clean up main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Services from './pages/Services'
-import { Route } from 'react-router-dom'
 
 createRoot(document.getElementById("root")!).render(<App />);
